b393eb539ec3fcc708f62d53d02cfe84e7a58d12
Stephen Schaub <sschaub@gmail.com>
2020-03-10 12:24:49 -0400

    Add lab 8

---
+++ lab8/src/main/java/App.java
@@ -0,0 +1,11 @@
+/*
+ * This class contains the main method of the application
+ */
+
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ lab8/src/main/java/Lab8.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Lab8 {
+    public static String findFirstNonEmpty(List<String> names) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public static String findShortest(String[] names) {
+        // Tip: Use Stream.of() to create a Stream from an array
+        throw new RuntimeException("Not implemented");
+    }
+
+    public static int countNullOrEmpty(List<String> names) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public static void removeNullOrEmpty(List<String> names) {
+        // Tip: Use ArrayList.removeIf()
+        throw new RuntimeException("Not implemented");
+    }
+
+    public static double computeAverageWeight(List<Gopher> gophers) {
+        // Tip: use mapToDouble() and average()
+        throw new RuntimeException("Not implemented");
+    }
+
+}
